898ca26d80b16d23a4f556ab6d80d62582bdc98a
Gülsüm Erdogan <guls0489@edu.zealand.dk>
2023-06-11 14:56:42 +0200

    To klasser added med instance fields, properties og constructors :D

---
+++ ZealandClimate/Lokale.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZealandClimate
+{
+    internal class Lokale
+    {
+
+        public int id;
+        public string lokaleNummer;
+        public int maxAntalPersoner;
+
+
+        public Lokale(int id, string lokaleNummer, int maxAntalPersoner)
+        {
+            ID = id;
+            LokaleNummer = lokaleNummer;
+            MaxAntalPersoner = maxAntalPersoner;
+        }
+
+        public int ID { get; set; }
+        public string LokaleNummer { get; set; }
+        public int MaxAntalPersoner { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"Lokalenummer info: {ID}, {LokaleNummer}, {MaxAntalPersoner}";
+        }
+    }
+}
---
+++ ZealandClimate/Måling.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZealandClimate
+{
+    internal class Måling : Lokale 
+    {
+        //properties 
+
+        public int id;
+        public DateTime dateTime;
+        public double temp;
+        public int co2;
+
+        //constructor
+        public Måling(int ID, DateTime DateTime, double Temp, int Co2)
+        {
+            ID = id;
+            DateTime = dateTime;
+            Temp = temp;
+            Co2 = co2;
+        }
+
+        //getters and setters (prop)
+        public int ID { get; set; }
+        public DateTime DateTime { get; set; }
+        public double Temp { get; set; }
+
+        public int Co2 { get; set; }
+
+        // ToString method 
+
+        public override string ToString()
+        {
+            return $"Måling: {ID}, {DateTime}, {Temp}"; 
+        }
+    }
+}
